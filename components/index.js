2f8784fae6030fba83f7322fd475316c8197e780
B. Gailītis <broks.gailitis@gmail.com>
2024-06-04 15:00:20 +0300

    responsive fixes

--- components/index.js
+++
@@ -1,5 +0,0 @@
-import { Navigation } from "./navigation/index.js";
-import { HeroSection } from "./hero/index.js";
-
-window.customElements.define('bg-navigation', Navigation);
-window.customElements.define('bg-hero-section', HeroSection);
